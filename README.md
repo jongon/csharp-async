715f406523ac2df9f10749590ad49e07abf23baa
Jonathan Gonzalez <jon.gon1@gmail.com>
2016-11-29 08:38:07 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Async and await example with c#
+
+This is an example with c# test asynchronous methods
---
+++ TestAsync/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestAsync
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var demo = new AsyncAwaitDemo();
+            demo.DoStuff();
+
+            while (true)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("Doing Stuff on the Main Thread...................");
+            }
+        }
+        public class AsyncAwaitDemo
+        {
+            public async Task<string> DoStuff()
+            {
+                return await Task.Run(() => LongRunningOperation());
+            }
+
+            private static string LongRunningOperation()
+            {
+                int counter;
+
+                for (counter = 0; counter < 5000000; counter++)
+                {
+                    Console.WriteLine(counter);
+                    Thread.Sleep(100);
+                }
+
+                return "Counter = " + counter;
+            }
+        }
+    }
+}
